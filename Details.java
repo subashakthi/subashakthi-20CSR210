a74135add4eee081bc426e54831f6ec851154179
subashakthi <91017224+subashakthi@users.noreply.github.com>
2021-10-23 11:10:59 +0530

    First commit

---
+++ Details.java
@@ -0,0 +1,24 @@
+class student 
+{
+   public static void main(String args[])
+   {
+     string Name="suba";
+     string rollno="20csr210";
+     string father_name="thiru";
+     string mother_name="poongodi";
+     int age=18;
+     int mobile=9788291848";
+     string email="subashakthi102@gmail.com";
+     string blood_group="o+";
+     string address="tirupur";
+     int adhar_no=90399435;
+     System.out.println(Name);
+     System.out.println(rollno);
+     System.out.println(father_name);
+     System.out.println(mother_name);
+     System.out.println(age);
+     System.out.println(mobile);
+     System.out.println(email);
+     
+   }
+}
